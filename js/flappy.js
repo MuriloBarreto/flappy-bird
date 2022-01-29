6b3a96bf72b5eeefbbedeb3b21a296a9ef55f18a
Murilo Barreto <barreto_11almeida@hotmail.com>
2022-01-29 14:02:28 -0300

    adicionando barreiras ao jogo

--- js/flappy.js
+++ js/flappy.js
@@ -15,6 +15,35 @@ function Barreira(reversa = false){
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
-const b = new Barreira(true)
-b.setAltura(200)
+// const b = new Barreira(true)
+// b.setAltura(200)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x){
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    
+    this.setX = x => this.elemento.style.left = `${x}px`
+
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+
+const b = new ParDeBarreiras(700, 500, 800)
 document.querySelector('[wm-flappy]').appendChild(b.elemento)
